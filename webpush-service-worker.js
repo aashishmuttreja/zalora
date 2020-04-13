8ef93b16f4766a3db8d097fd182fbe9ac4bbeff9
aashishmuttreja <50440645+aashishmuttreja@users.noreply.github.com>
2020-04-13 17:02:12 +0530

    first commit

---
+++ webpush-service-worker.js
@@ -0,0 +1,3 @@
+importScripts('https://api.pushio.com/webpush/sdk/service_min.js');
+var config = '{"appserviceKey":"BEQLoFtTGZ0tlhPv0kguoMWfSUuwGHtJrf0dfzl24VffvnUNC1nZJaHqSKf0j_yyMJcobXsFAvXM0VmelCY5hPE=","apiKey":"ABEjL7oBuw8Fw0-svzbK5DrOs","accountToken":"ABEq_61sebCfTlmtGZ-mOxe88","appver":"0.0.0","apiHost":"https://api.pushio.com","lazy":false}';
+orawp.init(config);
